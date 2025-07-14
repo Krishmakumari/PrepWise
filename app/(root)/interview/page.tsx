d7477884b8cca3508bb109b3fc6df3a0219fa373
Krishma Kumari <krishma24604@gmail.com>
2025-07-14 18:43:18 +0530

    updated the interview generation page

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import Agent from '@/components/Agent'
+import { getCurrentUser } from '@/lib/actions/auth.action'
 
+const Page = async () => {
+  const user = await getCurrentUser();
 
-const Page = () => {
+  if (!user) return <p className="text-center mt-10">Please sign in to continue.</p>;
+
   return (
     <>
-
-    <h3> Interview Generation</h3>
-
-    <Agent userName="You" userId="user1" type="generate"/>
+      <h3>Interview Generation</h3>
+      <Agent userName={user.name} userId={user.id} type="generate" />
     </>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
